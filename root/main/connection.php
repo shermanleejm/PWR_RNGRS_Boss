3d4a52ab3ab0c41d7e71413d8b558d3bbacdd882
Sherman <leesherman@Sherman.local>
2019-08-27 13:38:45 +0800

    added database files

---
+++ root/main/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+class ConnectionManager {
+    public function getConnection() {
+        $credentials = parse_ini_file( ($_SERVER["DOCUMENT_ROOT"] . "/../private/database.ini") ) ;
+
+        $conn = mysqli_connect($credentials["host"], $credentials["username"], $credentials["password"], $credentials["dbname"]);
+
+        return $conn;
+    }
+}
+
+$dao = new ConnectionManager();
+$pdo = $dao->getConnection();
+$sql = "DELETE table table1";
+$pdo->
+
+
+
+?>
---
+++ root/main/homepage.php
@@ -0,0 +1,12 @@
+<html>
+
+<h1 align="center">Welcome to Boss Bidding</h1>
+<div align="center">
+<form action="processlogin.php" method="POST">
+    Username:<input type="text" name="username">
+    Passord:<input type="password" name="password">
+    <input type="submit">
+</form>
+</div>
+
+</html>
